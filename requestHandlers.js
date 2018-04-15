9d120d8ed70ce277b683134b39f6e43fec95cfb4
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-15 09:58:16 -0700

    Finished, but not able to get an image to show

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,6 @@
 const qs = require('querystring');
 const fs = require('fs');
+const formidable = require('formidable');
 
 function start(response, postData) {
     console.log('Request handler \'start\' was called.');
@@ -10,9 +11,10 @@ function start(response, postData) {
         'charset=UTF-8" />' +
         '</head>' +
         '<body>' +
-        '<form action="/upload" method="post">' +
-        '<textarea name="text" rows="20" cols="60"></textarea>' +
-        '<input type="submit" value="Submit text" />' +
+        '<form action="/upload" enctype ="multipart/form-data" ' +
+        'method="post">' +
+        '<input type= "file" name="upload" multiple="multiple"> ' +
+        '<input type="submit" value="Upload File" />' +
         '</form>' +
         '</body>' +
         '</html>';
@@ -22,17 +24,27 @@ function start(response, postData) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log('Request handler \'upload\' was called.');
+
+    const form = new formidable.IncomingForm();
+    console.log('About to parse');
+    form.parse(request, function(err, fields, files) {
+        if(err) {
+            fs.unlink('/tmp/test.png');
+            fs.rename(files.upload.path, '/tmp/test.png');
+        }
+    });
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('You sent' + qs.parse(postData).text);
+    response.write("Received image: <br />");
+    response.write("<img src='/show' />");
     response.end();
 }
 
 function show(response) {
     console.log('Request handler \'show\' was called.');
     response.writeHead(200, { 'Content-Type': 'image/png' });
-    fs.createReadStream('tmp/test.png').pipe(response);
+    fs.createReadStream('/tmp/test.png').pipe(response);
 }
 
 exports.start = start;
